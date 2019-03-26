3b6ff2c10f78a2c710efbaba3d1ba370ea63c640
Simon Todeschini <simon.todeschini@viacesi.fr>
2019-03-26 22:50:16 +0100

    feat(#2): Hide important information on API returns

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,15 +1,11 @@
 export class User {
     id: number;
     username: string;
-    last_name: string;
-    first_name: string;
-    email: string;
+    last_login: Date;
 
     constructor(obj?: any) {
         this.id             = obj && obj.id             || null;
         this.username       = obj && obj.username       || null;
-        this.last_name      = obj && obj.last_name      || null;
-        this.first_name     = obj && obj.first_name     || null;
-        this.email          = obj && obj.email          || null;
+        this.last_login     = obj && obj.last_login     || null;
     }
 }
